81657631a47cef14b5de85c5bde922ce1de43ab8
Brian <b.perez3237@gmail.com>
2022-02-06 16:07:03 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+  if (n < 400) {
+    return 'This one is on me!';
+  } else if (n > 2000 && n <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (n >= 2500) {
+    return 'No can do.';
+  }
+  return
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(c){
   // Write your code here!
+  return c==='NYC' ? 'Ok, sounds good.' : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(s){
   // Write your code here!
+  switch(s) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+    
+  }
+
 }
